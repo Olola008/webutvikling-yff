b54e872b13d04aa1135f8f4984e864576779bd59
Olola008 <Olola008@osloskolen.no>
2026-09-16 14:56:27 +0200

    La til kategorier for inntekter og utgifter

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ console.log("javascript herrrr");
 const inntektInput = document.getElementById("inntekt");
 const inntektKnapp = document.getElementById("inntektKnapp");
 const inntektSum = document.getElementById("inntektSum");
+const inntektType = document.getElementById("inntektType");
 
 const utgiftInput = document.getElementById("utgift");
 const utgiftKnapp = document.getElementById("utgiftKnapp");
 const utgiftSum = document.getElementById("utgiftSum");
+const utgiftType = document.getElementById("utgiftType");
 
 const saldo = document.getElementById("saldo");
 
@@ -18,6 +20,13 @@ let totalUtgift = 0;
 
 inntektKnapp.addEventListener("click", function () {
   const beløp = Number(inntektInput.value);
+  const type = inntektType.value;
+  const nyPost = document.createElement("li");
+
+  nyPost.textContent = type + ": " + beløp + " kr";
+  document.getElementById("postListe").appendChild(nyPost);
+
+  console.log(inntektType.value);
 
   if (inntektInput.value === "") {
     alert("Feltet er tomt");
@@ -44,6 +53,11 @@ inntektKnapp.addEventListener("click", function () {
 
 utgiftKnapp.addEventListener("click", function () {
   const beløp = Number(utgiftInput.value);
+  const type = utgiftType.value;
+  const nyPost = document.createElement("li");
+
+  nyPost.textContent = type + ": " + beløp + " kr";
+  document.getElementById("postListe").appendChild(nyPost);
 
   totalUtgift += beløp;
 
